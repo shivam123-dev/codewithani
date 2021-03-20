b88167601ff6c55050b4bdbc79e09678146e1867
aniket48020 <81038933+aniket48020@users.noreply.github.com>
2021-03-20 23:22:01 +0530

    Create averageof_n_no.cpp

---
+++ averageof_n_no.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a,s=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        s=s+a;
+    }
+    cout<<s/n;
+    return 0;
+
+
+
+}
